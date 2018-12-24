fc90c699f33cd83aa0c78e0784414fd41557ba79
bhadresh-goswami <tops.bhadresh@gmail.com>
2018-12-24 12:43:05 +0530

    done
    done

---
+++ ReadAndReadLine/ReadAndReadLine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAndReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a;
+            //int b;
+
+            //Console.Write("Enter Int in a:\t");
+            ////need to cast in ReadLine()
+            //a = Convert.ToInt16(Console.ReadLine());
+
+
+            //Console.Write("Enter Int in b:");
+            //b = Console.Read();
+
+            //Console.WriteLine("\n\n\tValue of a is:" + a);
+            //Console.WriteLine("\n\n\tValue of a is:{0}\n\n", b);
+
+
+
+            //Console.WriteLine("Press any key for exit.");
+
+            //Console.ReadKey();//press any key for exit
+
+
+            //valid identifier names
+            int a;
+            int marks;
+            int rank;
+            double res_marks;
+        }
+    }
+}
---
+++ Session_1_BasicWithCSharp/Session_1_BasicWithCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_1_BasicWithCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome, Students!\n\n");
+
+            Console.WriteLine("Hello, ");
+            Console.WriteLine("Student!");
+
+            Console.ReadLine();
+        }
+    }
+}
